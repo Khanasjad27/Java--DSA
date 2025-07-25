0fb92ff55f49a4bbd829a3d981a2d8f645f72227
Khan Asjad Shamshad Ahmed <khanasjadshamshadahmed@gmail.com>
2025-07-25 23:44:52 +0530

    Day24

--- Day24 (Queue)/StackUsingTwoQueue.java
+++ Day24 (Queue)/StackUsingTwoQueue.java
@@ -27,9 +27,64 @@ public class StackUsingTwoQueue {
             }
 
             int top = -1;
+
+            // case1=> jisme q1 me saare elements hai
+            if (!q1.isEmpty()) { // check karre hai ki q1 me elemnt hai ya q2 me
+                while (!q1.isEmpty()) {  // jab tak q1 empty nhi hota tabtak q1 se elemnt ko nikalege
+                    top = q1.remove();  // yaha pe loop end hote hote humko last element miljaaega
+                    if(q1.isEmpty()){ // jaise hi q1 empty hojaaega waise hi break kardege last me top return kardege coz top me last element ke val store hai
+                        break;
+                    }
+                    q2.add(top); // agar last elemt nhi hai to q2 me hum add karte jaaege
+                }
+            }else{ // case2 => isme q2 me saare elemnts hai
+                // same as above but wrt q2
+                while (!q2.isEmpty()) {
+                    top = q2.remove();
+                    if(q2.isEmpty()){
+                        break;
+                    }
+                    q1.add(top);
+                }
+            }
+
+            return top;
+        }
+
+        public static int peek(){
+            if(isEmpty()){
+                System.out.print("Queue is empty");
+                return -1;
+            }
+
+            int top = -1;
+
+            // case1=> jisme q1 me saare elements hai
+            if (!q1.isEmpty()) { // check karre hai ki q1 me elemnt hai ya q2 me
+                while (!q1.isEmpty()) {  // jab tak q1 empty nhi hota tabtak q1 se elemnt ko nikalege
+                    top = q1.remove();  // yaha pe loop end hote hote humko last element miljaaega
+                    q2.add(top); // agar last elemt nhi hai to q2 me hum add karte jaaege
+                }
+            }else{ // case2 => isme q2 me saare elemnts hai
+                // same as above but wrt q2
+                while (!q2.isEmpty()) {
+                    top = q2.remove();
+                    q1.add(top);
+                }
+            }
+
+            return top;
         }
     }
     public static void main(String[] args) {
-        
+        Stack s = new Stack();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+
+        while (!s.isEmpty()) {
+            System.out.println(s.peek());
+            s.pop();
+        }
     }
 }
