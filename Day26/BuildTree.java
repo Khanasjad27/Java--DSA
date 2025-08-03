c5eff1096facbc9cc1e0d9cf67b05e59a2a7217c
Khan Asjad Shamshad Ahmed <khanasjadshamshadahmed@gmail.com>
2025-08-03 18:04:55 +0530

    Day26

--- Day26/BuildTree.java
+++ Day26/BuildTree.java
@@ -61,6 +61,37 @@ public class BuildTree {
             postOrder(root.right);
             System.out.print(root.data+" "); 
         }
+
+        // Level order
+        public static void levelOrder(Node root){
+            if(root == null){
+                return;
+            }
+
+            Queue<Node> q = new LinkedList<>();
+            q.add(root);
+            q.add(null);
+
+            while(!q.isEmpty()){
+                Node currNode = q.remove();
+                if(currNode == null){
+                    System.out.println();
+                    if(q.isEmpty()){
+                        break;
+                    }else{
+                        q.add(null);
+                    }
+                }else{
+                    System.out.print(currNode.data+" ");
+                    if(currNode.left!=null){
+                        q.add(currNode.left);
+                    }
+                    if(currNode.right!=null){
+                        q.add(currNode.right);
+                    }
+                }
+            }
+        }
     }
     public static void main(String[] args) {
         int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
@@ -80,5 +111,9 @@ public class BuildTree {
         // Inorder
         System.out.print("PostOrder: ");
         tree.postOrder(root);
+        // LevelOrder
+        System.out.println();
+        System.out.println("LevelOrder: ");
+        tree.levelOrder(root);
     }
 }
