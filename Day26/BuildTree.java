dcfb7c90a1707c528f471983e7cc7d991305a6d1
Khan Asjad Shamshad Ahmed <khanasjadshamshadahmed@gmail.com>
2025-08-03 15:57:23 +0530

    Day26

--- Day26/BuildTree.java
+++ Day26/BuildTree.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class BuildTreePreorder {
+public class BuildTree {
     static class Node{
         int data;
         Node left;
@@ -15,6 +15,8 @@ public class BuildTreePreorder {
 
     static class BinaryTree{
         static int idx = -1;
+
+        // root node printing
         public static Node buildTree(int nodes[]){
             idx++;
             if(nodes[idx] == -1){
@@ -26,12 +28,27 @@ public class BuildTreePreorder {
 
             return newNode;
         }
+
+        // preOrder
+        public static void preOrder(Node root){
+            if(root == null){
+                return;
+            }
+
+            System.out.print(root.data+" ");
+            preOrder(root.left);
+            preOrder(root.right);
+        }
     }
 
     public static void main(String[] args) {
         int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
         BinaryTree tree = new BinaryTree();
         Node root = tree.buildTree(nodes);
-        System.out.println(root.data);
+        //  root
+        // System.out.println(root.data);
+
+        // preOrder
+        tree.preOrder(root);
     }
 }
