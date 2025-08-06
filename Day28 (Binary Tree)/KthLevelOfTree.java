b5cd05c425a1b1c17c8a1b8e59fe98bc5951d380
Khan Asjad Shamshad Ahmed <khanasjadshamshadahmed@gmail.com>
2025-08-06 15:14:34 +0530

    Day28

---
+++ Day28 (Binary Tree)/KthLevelOfTree.java
@@ -0,0 +1,66 @@
+import java.util.*;
+
+public class KthLevelOfTree {
+    static class Node{
+        int data;
+        Node left, right;
+        Node(int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    // Approch 1  => Level based approch
+    public static void kthLevel1(Node root, int k){
+        Queue<Node> q = new LinkedList<>();
+        int level = 1;
+        if(root == null){
+            return;
+        }
+        q.add(root);
+        q.add(null);
+
+        while(!q.isEmpty()){
+            Node curr = q.remove();
+            if(curr == null){
+                System.out.println();
+                level++;
+                if(q.isEmpty()){
+                    break;
+                }else{
+                    q.add(null);
+                }
+            }
+            else{
+                if(level == k){
+                    System.out.print(curr.data+" ");
+                }
+                if(curr.left!=null){
+                    q.add(curr.left);
+                }
+                if(curr.right!=null){
+                    q.add(curr.right);
+                }
+            }
+        }
+    }
+    public static void main(String[] args) {
+        /*
+                    1
+                   / \
+                  2   3  
+                /  \ / \
+               4   5 6  7  
+         */
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right = new Node(3);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+        System.out.print("Nodes at kth level are: ");
+        kthLevel1(root, 3);
+    }
+}
