38860fcf3cb003d967382c8c5e9ac1aacf9eaa23
Khan Asjad Shamshad Ahmed <khanasjadshamshadahmed@gmail.com>
2025-08-08 09:04:44 +0530

    Day28

--- Day28 (Binary Tree)/LowerstCommonAncestor.java
+++ Day28 (Binary Tree)/LowerstCommonAncestor.java
@@ -1,7 +1,77 @@
 import java.util.*;
 
 public class LowerstCommonAncestor {
-    public static void main(String[] args) {
+    static class Node{
+        int data;
+        Node left, right;
+        Node(int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static boolean getPath(Node root, int n, ArrayList<Node> path){
         
+        // Base case
+        if(root == null){
+            return false;
+        }
+
+        path.add(root);
+
+        if(root.data == n){
+            return true;
+        }
+
+        //now for left n right subtree
+        boolean fLeft = getPath(root.left, n, path);
+        boolean fRight = getPath(root.right, n, path);
+
+        if(fLeft || fRight){
+            return true;
+        }
+        
+        path.remove(path.size()-1);
+        return false;
+    }
+
+    public static Node lca(Node root,int n1, int n2){
+        ArrayList<Node> path1 = new ArrayList<>();
+        ArrayList<Node> path2 = new ArrayList<>();
+
+        getPath(root, n1, path1);
+        getPath(root, n2, path2);
+
+        int i=0;
+        for(;i<path1.size() && i<path2.size();i++){
+            if(path1.get(i) != path2.get(i)){
+                break;
+            }
+        }
+
+        Node lca = path1.get(i-1);
+        return lca;
+
+    }
+    public static void main(String[] args) {
+         /*
+                    1
+                   / \
+                  2   3  
+                /  \ / \
+               4   5 6  7  
+         */
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right = new Node(3);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        int n1 = 7, n2 = 5;
+
+        System.out.print(lca(root, n1, n2).data);
     }
 }
